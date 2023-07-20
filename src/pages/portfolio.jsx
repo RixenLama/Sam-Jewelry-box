edbeba4d2af0968121e06045b0b7237a9d59b8f7
Rixen <Rixenlama@gmail.com>
2023-07-20 11:10:28 +0300

    web update

---
+++ src/pages/portfolio.jsx
@@ -0,0 +1,78 @@
+
+const Portfolio = () =>{
+    return(
+        <div className="fixer">
+            <div className="portfolio-bg">
+                <div className="container">
+                    
+                    <h2 className="portfolio-header TEXTCENTER DBLUE ONE">Previous Collections</h2>
+                    <div className="portfolio-container">
+                        <div className="portfolio-item">
+                            <img className="portfolio-img" src="./portfolio-1.png" alt="" />
+                        </div>
+                        <div className="portfolio-item">
+                            <img className="portfolio-img" src="./portfolio-2.png" alt="" />
+                        </div>
+                        <div className="portfolio-item">
+                            <img className="portfolio-img" src="./portfolio-3.png" alt="" />
+                        </div>
+                        <div className="portfolio-item">
+                            <img className="portfolio-img" src="./portfolio-4.png" alt="" />    
+                        </div>
+                        <div className="portfolio-item">
+                            <img className="portfolio-img" src="./portfolio-1.png" alt="" />
+                        </div>
+                        <div className="portfolio-item">
+                            <img className="portfolio-img" src="./portfolio-2.png" alt="" />
+                        </div>
+                        <div className="portfolio-item">
+                            <img className="portfolio-img" src="./portfolio-3.png" alt="" />
+                        </div>
+                        <div className="portfolio-item">
+                            <img className="portfolio-img" src="./portfolio-4.png" alt="" />    
+                        </div>
+                        <div className="portfolio-item">
+                            <img className="portfolio-img" src="./portfolio-1.png" alt="" />
+                        </div>
+                        <div className="portfolio-item">
+                            <img className="portfolio-img" src="./portfolio-2.png" alt="" />
+                        </div>
+                        <div className="portfolio-item">
+                            <img className="portfolio-img" src="./portfolio-3.png" alt="" />
+                        </div>
+                        <div className="portfolio-item">
+                            <img className="portfolio-img" src="./portfolio-4.png" alt="" />    
+                        </div>
+                        <div className="portfolio-item">
+                            <img className="portfolio-img" src="./portfolio-1.png" alt="" />
+                        </div>
+                        <div className="portfolio-item">
+                            <img className="portfolio-img" src="./portfolio-2.png" alt="" />
+                        </div>
+                        <div className="portfolio-item">
+                            <img className="portfolio-img" src="./portfolio-3.png" alt="" />
+                        </div>
+                        <div className="portfolio-item">
+                            <img className="portfolio-img" src="./portfolio-4.png" alt="" />    
+                        </div>
+                        <div className="portfolio-item">
+                            <img className="portfolio-img" src="./portfolio-1.png" alt="" />
+                        </div>
+                        <div className="portfolio-item">
+                            <img className="portfolio-img" src="./portfolio-2.png" alt="" />
+                        </div>
+                        <div className="portfolio-item">
+                            <img className="portfolio-img" src="./portfolio-3.png" alt="" />
+                        </div>
+                        <div className="portfolio-item">
+                            <img className="portfolio-img" src="./portfolio-4.png" alt="" />    
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Portfolio
--- src/pages/website.jsx
+++ src/pages/website.jsx
@@ -1,6 +1,7 @@
 import NavBar from "../components/nav-bar";
 import Hero from "./hero";
 import AboutMe from "./about-me";
+import Portfolio from "./portfolio";
 
 const Web = () =>{
     return(
@@ -8,6 +9,7 @@ const Web = () =>{
             <NavBar />
             <Hero />
             <AboutMe />
+            <Portfolio />
         </>
     )
 }
